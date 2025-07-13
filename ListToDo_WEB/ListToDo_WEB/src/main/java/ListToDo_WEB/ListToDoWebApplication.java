64f34c300e9bc6776f83e3de267735034a801538
Bernardo de Salles Cruz <168240480+devSalles@users.noreply.github.com>
2025-07-13 12:45:29 -0300

    Add files via upload

---
+++ ListToDo_WEB/ListToDo_WEB/src/main/java/ListToDo_WEB/ListToDoWebApplication.java
@@ -0,0 +1,13 @@
+package ListToDo_WEB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListToDoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListToDoWebApplication.class, args);
+	}
+
+}
---
+++ ListToDo_WEB/ListToDo_WEB/src/test/java/ListToDo_WEB/ListToDoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package ListToDo_WEB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListToDoWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
