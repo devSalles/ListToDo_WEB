c74394017f6a638e97ddfdd078fb54a91343a1d3
Bernardo de Salles Cruz <sallesbernardo89@gmail.com>
2025-07-14 16:20:13 -0300

    Foi feito os metodos da controller de cadastro e login e os metodos de busca no repositorio

--- ListToDo_WEB/ListToDo_WEB/src/main/java/ListToDo_WEB/controller/User/CadastroController.java
+++ ListToDo_WEB/ListToDo_WEB/src/main/java/ListToDo_WEB/controller/User/CadastroController.java
@@ -1,4 +1,61 @@
 package ListToDo_WEB.controller.User;
 
+import ListToDo_WEB.dto.UserDTO;
+import ListToDo_WEB.model.UserModel;
+import ListToDo_WEB.repository.UserRepository;
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/user")
 public class CadastroController {
+
+    private final UserRepository userRepository;
+
+    public CadastroController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/cadastro")
+    public ModelAndView cadastroTemplate()
+    {
+        ModelAndView mv = new ModelAndView("cadastro/cadastro");
+        mv.addObject("userDTO", new UserDTO());
+        return mv;
+    }
+
+    @PostMapping("/cadastro")
+    public ModelAndView cadastroReq(@Valid @ModelAttribute("userDTO") UserDTO userDTO, BindingResult bindingResult)
+    {
+        ModelAndView mv = new ModelAndView("cadastro/cadastro");
+
+        if(bindingResult.hasErrors())
+        {
+            return mv;
+        }
+
+        if(userRepository.findByEmail(userDTO.getEmail()) != null)
+        {
+            mv.addObject("erro", "email já esta em uso");
+            return mv;
+        }
+
+
+        if(userRepository.findBySenha(userDTO.getSenha()) != null)
+        {
+            mv.addObject("erro", "senha já esta em uso");
+            return mv;
+        }
+
+        UserModel userSave=userDTO.toUser();
+        this.userRepository.save(userSave);
+
+        return new ModelAndView("redirect:/user/login");
+    }
 }
--- ListToDo_WEB/ListToDo_WEB/src/main/java/ListToDo_WEB/controller/User/LoginController.java
+++ ListToDo_WEB/ListToDo_WEB/src/main/java/ListToDo_WEB/controller/User/LoginController.java
@@ -1,4 +1,44 @@
 package ListToDo_WEB.controller.User;
 
+import ListToDo_WEB.model.UserModel;
+import ListToDo_WEB.repository.UserRepository;
+import org.springframework.boot.Banner;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/user")
 public class LoginController {
+
+    private final UserRepository userRepository;
+
+    public LoginController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/login")
+    public ModelAndView loginTemplate()
+    {
+        return new ModelAndView("login/login");
+    }
+
+    @PostMapping("/login")
+    public ModelAndView loginReq(@RequestParam String email, @RequestParam String senha)
+    {
+        UserModel userModel = this.userRepository.findByEmail(email);
+        if(userModel != null && userModel.getEmail().equals(email) && userModel.getSenha().equals(senha))
+        {
+            return new ModelAndView("tarefa/home");
+        }
+        else
+        {
+            ModelAndView mv = new ModelAndView("login/login");
+            mv.addObject("erro", "email ou senha incorreto");
+            return mv;
+        }
+    }
 }
